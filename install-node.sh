a63772b8e4e20827a2abe93f7feb9bbf9f70e10e
Coopyy <pat.pvp.unturned@gmail.com>
2018-02-16 17:38:37 -0600

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -5,6 +5,7 @@ printf "\033c"
 mkdir "/home/NodeJS App"
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install nodejs
 npm install -g pm2
 echo '-------------------------------FINISHED-------------------------------'
 echo 'Add any NPM packages you would like. Press ENTER after each one. Once you are finished or do not want to add anything, leave the field blank.'
