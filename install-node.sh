10632a77b194c83495fda337aeeb0b595f297d3f
Coopyy <pat.pvp.unturned@gmail.com>
2018-02-16 17:38:07 -0600

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -8,12 +8,16 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 npm install -g pm2
 echo '-------------------------------FINISHED-------------------------------'
 echo 'Add any NPM packages you would like. Press ENTER after each one. Once you are finished or do not want to add anything, leave the field blank.'
+echo
 read package
-while [ -n $package ]
+while [ $package != "" ]
 do
     npm install -g $package
+    echo 'Enter another package, or leave the field blank.' 
     read package
 done
+
+echo 'All finished!'
 
 else echo Please run this as Root user.
 fi
