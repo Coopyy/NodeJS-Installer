9dd39bd6ae1aba905768b9e41e87a3973464dc48
Coopyy <pat.pvp.unturned@gmail.com>
2018-02-16 17:53:04 -0600

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -9,15 +9,13 @@ apt-get install nodejs
 npm install -g pm2
 clear
 echo '-------------------------------FINISHED-------------------------------'
-echo 'Add any NPM packages you would like. Press ENTER after each one. Once you are finished or do not want to add anything, leave the field blank.'
-echo
-read package
-while [ "$package" != "" ]
+echo 'Add any NPM packages you would like.\n Press ENTER after each one.\n Once you are finished or do not want to add anything, leave the field blank.\n'
+read "Package Name:  " package
+while [[ "$package" != "" ]]
 do
     npm install -g $package
-    echo 'Enter another package, or leave the field blank.' 
-    echo
-    read package
+    echo 'Enter another package, or leave the field blank.\n' 
+    read "Package Name:  " package
 done
 
 echo 'All finished!'
